e54c1f93427669b2cf593b499ebf1c2098121f35
nata848498 <natakiyqwe@gmail.com>
2025-10-30 00:31:26 +0200

    Додано задачу 2 (середнє арифметичне)

---
+++ ВведенняДоФаху/Лабораторна1/задача2/lab1_task2/lab1_task2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    double a, b;
+    cout << "Введіть два числа: ";
+    cin >> a >> b;
+    cout << "Середнє арифметичне: " << (a + b) / 2 << endl;
+    return 0;
+}
