95f0cac8f8b908caf6e45a748246acda84b7af8c
nata848498 <natakiyqwe@gmail.com>
2025-10-30 00:19:46 +0200

    Додано README для задачі 1 та код лабораторної

---
+++ ВведенняДоФаху/Лабораторна1/задача1/lab1_task1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "¬вед≥ть два числа: ";
+    cin >> a >> b;
+    cout << "—ума: " << a + b << endl;
+    return 0;
+}
---
+++ ВведенняДоФаху/Лабораторна1/задача1/lab1_task1/lab1_task1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "ukr");
+    int a, b;
+    cout << "Введіть два числа: ";
+    cin >> a >> b;
+    cout << "Сума: " << a + b << endl;
+    return 0;
+}
